c28483d87dae28aad93bc6e78ebc3095438cc6c4
h573042 <jasmine.erlandsen@hotmail.com>
2024-11-01 14:30:45 +0100

    les blogg

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -21,8 +21,46 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
-
+		Blogg samling = null;
 		
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(mappe + "/" + filnavn))) {
+			String førstelinje = reader.readLine();
+			int linje = Integer.parseInt(førstelinje);
+			
+			samling = new Blogg(linje);
+			for(int i = 0; i < linje; i++) {
+				String innhold = reader.readLine();
+				
+				if(innhold.equals(TEKST)) {
+					int id = Integer.parseInt(reader.readLine().trim());
+					String bruker = reader.readLine().trim();
+					String dato = reader.readLine().trim();
+					int likes = Integer.parseInt(reader.readLine().trim());
+					String tekst = reader.readLine().trim();
+					
+					Tekst innlegg = new Tekst(id, bruker, dato, likes, tekst);
+					samling.leggTil(innlegg);
+					
+				}else if(innhold.equals(BILDE)) {
+					int id = Integer.parseInt(reader.readLine().trim());
+					String bruker = reader.readLine().trim();
+					String dato = reader.readLine().trim();
+					int likes = Integer.parseInt(reader.readLine().trim());
+					String tekst = reader.readLine().trim();
+					String url = reader.readLine().trim();
+					
+					Bilde innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
+					samling.leggTil(innlegg);
+				}
+			}
 
+			
+		} catch(IOException e) {
+			System.out.println("Fil ikke innlegg: " + e.getMessage());
+			
+		}
+	return samling;
+		
 	}
 }
